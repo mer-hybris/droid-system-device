f3d202336c1e22e54ed30aea9408e36697325021
Franz-Josef Haider <franz.haider@jolla.com>
2018-07-05 09:57:57 +0200

    [dsd] Fix permissions of more files as required on Android 8. JB#42314
    Signed-off-by: Franz-Josef Haider <franz.haider@jolla.com>

--- helpers/fixup-permissions.sh
+++ helpers/fixup-permissions.sh
@@ -7,4 +7,9 @@ sudo chmod a+r $SYSTEM_MOUNT/etc/dhcpcd/dhcpcd-run-hooks
 sudo chmod a+r $SYSTEM_MOUNT/etc/partition_permission.sh
 sudo chmod a+r $SYSTEM_MOUNT/etc/wide-dhcpv6/dhcp6c.script
 sudo chmod a+r $SYSTEM_MOUNT/xbin/su
+sudo chmod a+r $SYSTEM_MOUNT/bin/logd
+sudo chmod a+r $SYSTEM_MOUNT/bin/webview_zygote32
+sudo chmod a+r $SYSTEM_MOUNT/bin/webview_zygote64
+sudo chmod a+r $SYSTEM_MOUNT/build.prop
+sudo chmod a+r $SYSTEM_MOUNT/etc/prop.default
 
