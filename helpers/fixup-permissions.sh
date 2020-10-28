e70d50ab2178fb3f4e7736da4a6d6c67dcdb8a83
Simonas Leleiva <simonas.leleiva@meramo.co.uk>
2020-10-28 06:55:49 +0000

    improve coding style
    Signed-off-by: Simonas Leleiva <simonas.leleiva@jolla.com>

--- helpers/fixup-permissions.sh
+++ helpers/fixup-permissions.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 # Add read permission for some binaries under system mount
 
 if [ "$SYSTEM_MOUNT" == "" ]; then
@@ -9,21 +11,21 @@ if [ "$SYSTEM_MOUNT" == "" ]; then
     exit 1
 fi
 
-sudo chmod a+r $SYSTEM_MOUNT/bin/run-as
-sudo chmod a+r $SYSTEM_MOUNT/bin/uncrypt
-sudo chmod a+r $SYSTEM_MOUNT/bin/netcfg
-sudo chmod a+r $SYSTEM_MOUNT/bin/install-recovery.sh
-sudo chmod a+r $SYSTEM_MOUNT/etc/dhcpcd/dhcpcd-run-hooks
-sudo chmod a+r $SYSTEM_MOUNT/etc/partition_permission.sh
-sudo chmod a+r $SYSTEM_MOUNT/etc/wide-dhcpv6/dhcp6c.script
-sudo chmod a+r $SYSTEM_MOUNT/xbin/su
-sudo chmod a+r $SYSTEM_MOUNT/bin/logd
-sudo chmod a+r $SYSTEM_MOUNT/bin/webview_zygote32
-sudo chmod a+r $SYSTEM_MOUNT/bin/webview_zygote64
-sudo chmod a+r $SYSTEM_MOUNT/build.prop
-sudo chmod a+r $SYSTEM_MOUNT/etc/prop.default
-sudo chmod a+r $SYSTEM_MOUNT/default.prop
-sudo chmod a+r $SYSTEM_MOUNT/bin/hw/android.hardware*
-sudo chmod a+r $SYSTEM_MOUNT/bin/secilc
-sudo chmod a+r $SYSTEM_MOUNT/etc/fs_config_dirs
+sudo chmod a+r "$SYSTEM_MOUNT"/bin/run-as
+sudo chmod a+r "$SYSTEM_MOUNT"/bin/uncrypt
+sudo chmod a+r "$SYSTEM_MOUNT"/bin/netcfg
+sudo chmod a+r "$SYSTEM_MOUNT"/bin/install-recovery.sh
+sudo chmod a+r "$SYSTEM_MOUNT"/etc/dhcpcd/dhcpcd-run-hooks
+sudo chmod a+r "$SYSTEM_MOUNT"/etc/partition_permission.sh
+sudo chmod a+r "$SYSTEM_MOUNT"/etc/wide-dhcpv6/dhcp6c.script
+sudo chmod a+r "$SYSTEM_MOUNT"/xbin/su
+sudo chmod a+r "$SYSTEM_MOUNT"/bin/logd
+sudo chmod a+r "$SYSTEM_MOUNT"/bin/webview_zygote32
+sudo chmod a+r "$SYSTEM_MOUNT"/bin/webview_zygote64
+sudo chmod a+r "$SYSTEM_MOUNT"/build.prop
+sudo chmod a+r "$SYSTEM_MOUNT"/etc/prop.default
+sudo chmod a+r "$SYSTEM_MOUNT"/default.prop
+sudo chmod a+r "$SYSTEM_MOUNT"/bin/hw/android.hardware*
+sudo chmod a+r "$SYSTEM_MOUNT"/bin/secilc
+sudo chmod a+r "$SYSTEM_MOUNT"/etc/fs_config_dirs
 
--- helpers/general-patches.sh
+++ helpers/general-patches.sh
@@ -1,13 +1,15 @@
+#!/bin/bash
+#
 # On Mediatek devices /dev/net permissions have been restricted for root only,
 # allow access also for others and group.
 # e.g. VPN clients needs access to /dev/net/tun with special user group.
 
-PATCH_FILE="init.wod.rc"
-echo "Finding $PATCH_FILE"
-PATCH_FILE_PATH=$(find $SYSTEM_SPARSE -type f -name $PATCH_FILE)
-if [ ! -z "$PATCH_FILE_PATH" ]; then
-  echo "Patching file $PATCH_FILE_PATH"
-  sed -i -e 's!mkdir /dev/net/ 0700 root root!mkdir /dev/net/ 0755 root root!g' $PATCH_FILE_PATH
+patch_file="init.wod.rc"
+echo "Finding $patch_file"
+patch_file_path=$(find "$SYSTEM_SPARSE" -type f -name $patch_file)
+if [ ! -z "$patch_file_path" ]; then
+  echo "Patching file $patch_file_path"
+  sed -i -e 's!mkdir /dev/net/ 0700 root root!mkdir /dev/net/ 0755 root root!g' "$patch_file_path"
 else
-  echo "File $PATCH_FILE does not exist, skipping..."
+  echo "File $patch_file does not exist, skipping..."
 fi
